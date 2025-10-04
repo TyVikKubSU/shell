24acf70dab8291010efa1d46c5275d2ccb172215
Alina <anitalozova@gmail.com>
2025-10-04 10:40:57 +0300

    Подключил историю команд

--- kubsh.c
+++ kubsh.c
@@ -4,16 +4,26 @@
 #include <string.h>
 
 #include <readline/readline.h>
+#include <readline/history.h>
+
+#define HISTORY_FILE ".kubsh_history"
 
 void debug(char *line) {
   printf("%s\n", line);
 }
 
 int main() {
+  read_history(HISTORY_FILE);
+
   char *input;
   
   while (true) {
     input = readline("$ ");
+
+    if (input == NULL) {
+      break;
+    }
+    add_history(input);
 
     if (!strcmp(input, "\\q")) {
       break;
@@ -22,7 +32,10 @@ int main() {
     } else {
       printf("%s: command not found\n", input);
     }
+    free(input);
   }
+
+  write_history(HISTORY_FILE);
   
   return 0;
 }
